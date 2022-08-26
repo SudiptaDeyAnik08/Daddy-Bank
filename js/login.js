a2c49cc569c558f1bed7cd429230683be1b56c33
SudiptaDeyAnik08 <sudiptadeysd06@gmail.com>
2022-08-26 11:18:40 +0600

    login Page complete

---
+++ js/login.js
@@ -0,0 +1,15 @@
+const submit_btn = document.getElementById('submit-btn');
+
+submit_btn.addEventListener('click',function(){
+    const email = document.getElementById('email').value;
+
+    const pass1 = document.getElementById('pass');
+    const pass_value = pass1.value;
+
+    if(email === 'a@gmail.com' && pass_value === '123'){
+        alert("login success");
+    }else{
+        alert("Can not login")
+    }
+    
+})
